711f6163f602eb46c939354914b6e1ccd284d080
Aarush2006 <aarushagrawal2006@gmail.com>
2019-01-13 18:03:14 +0530

    week_04 commit

---
+++ week_4/week_04 class.py
@@ -0,0 +1,55 @@
+print("arithmetic")
+
+a = 10
+b = 5
+c = a+b
+print(c)
+
+a = 10
+b = 5
+c = a-b
+print(c)
+
+a = 10
+b = 5
+c = a*b
+print(c)
+
+a = 10
+b = 5
+c = a/b
+print(c)
+
+a = 10
+b = 5
+c = a%b
+print(c)
+
+a = 10
+b = 5
+c = a**b
+print(c)
+
+c = 23**45
+print(c)
+
+print("comparison")
+
+print(3==10)
+print(3!=10)
+print(3<10)
+print(3>10)
+print(3<=10)
+print(3>=10)
+
+a = int(input("enter any number"))
+b = int(input("enter other number"))
+if a>b :
+    print("a is greater than b")
+    print("#THANK YOU FOR YOUR TIME#")
+else:
+    print("b is greater than a")
+    print("#THANK YOU FOR YOUR TIME#")
+
+
+
