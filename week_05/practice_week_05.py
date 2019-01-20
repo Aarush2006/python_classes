82b325e17084e4ca61c7426e8fe204eee8eff6bc
Aarush2006 <aarushagrawal2006@gmail.com>
2019-01-20 16:51:17 +0530

    week_05 commit

--- week_05/practice_week_05.py
+++ week_05/practice_week_05.py
@@ -6,3 +6,17 @@ print(list_hero[1])
 print(list_hero[2])
 print(list_hero[3])
 print(list_hero[4])
+
+list_villian= ["thanos", "darkside", "martian manhunter", "joker", "ultron"]
+print(list_villian[0])
+print(list_villian[1])
+print(list_villian[2])
+print(list_villian[3])
+print(list_villian[4])
+
+print("while loop")
+
+count= 0
+print("count", count)
+count= count + 1
+print("count : ", count)
