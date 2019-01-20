bdf6a06b0af567526ee8e8287bfc59cfa722267d
Aarush2006 <aarushagrawal2006@gmail.com>
2019-01-20 16:29:39 +0530

    first commit

---
+++ week_05/practice_week_05.py
@@ -0,0 +1,8 @@
+print("list")
+
+list_hero= ["batman", "superman", "spiderman", "thor", "vision"]
+print(list_hero[0])
+print(list_hero[1])
+print(list_hero[2])
+print(list_hero[3])
+print(list_hero[4])
