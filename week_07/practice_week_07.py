55e64c3610014a437e430a300b41df9ba15b0b37
Aarush2006 <aarushagrawal2006@gmail.com>
2019-02-03 17:46:08 +0530

    week_07 commit

--- week_07/practice_week_07.py
+++ week_07/practice_week_07.py
@@ -1,5 +1,52 @@
-hello= 'hello python world!'
-print(hello[2])
-print(hello[-15])
-print(hello[-17])
-print(hello[6:12])
+# hello= 'hello python world!'
+# print(hello[2])
+# print(hello[-15])
+# print(hello[-17])
+# print(hello[6:12])
+#
+# a= 'HELLO'
+# b= 'PYTHON'
+# print(a+b)
+# print(a+" "+b)
+# print(a+" "+b+" "+a)
+# print(a*5)
+#
+# if 'E' in a:
+#     print('E is present')
+# if 'E' in b:
+#     print('E is present')
+
+# whatever_list= ["2","57","afgehbm","true","h",".696","u75","4b","3","36.0"]
+# if "true" in whatever_list:
+#      print("true is there")
+# if "3456" not in whatever_list:
+#     print("3456 is not there")
+#
+# gaming_list= ["pubg","fortnite","minecraft","cod","fifa","w2k",]
+# if "cod" in gaming_list:
+#     print("cod is there")
+# else:
+#     print("cod not there")
+# if "nba2k" not in gaming_list:
+#     print("nba2k not there")
+# else:
+#     print("nba2k is there")
+#
+# the built in functions
+d= "i AM tHE HeRO"
+# print(d)
+# print(d.lower())
+# print(d.upper())
+# print(d.capitalize())
+# print(d.replace("AM", "qwertyuiopasdfghjklzxcvbnm"))
+
+def string_to_uppercase(d):
+    y= d.upper()
+    return y
+
+output= string_to_uppercase("i am a boy")
+print(output)
+
+whatever_list = ["cfgyud", "ertyyytfc", "afgehbm", "wedfgt", "asdfgh", "jsu"]
+for whatever in whatever_list:
+    print(string_to_uppercase(whatever))
